43c130a1a02a722163daed5e6248422666261d35
Joseph Diaz <diazjoseph366@gmail.com>
2026-02-06 22:54:35 -0800

    Push changes to trigger redeploy

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -3,6 +3,8 @@ import { redirect } from "next/navigation";
 import { createClient } from "@/lib/supabase/server";
 import { AccountActions } from "./AccountActions";
 
+export const dynamic = "force-dynamic";
+
 export default async function AccountPage() {
   const supabase = await createClient();
   const {
